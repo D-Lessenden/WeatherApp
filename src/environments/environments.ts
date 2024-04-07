cf8188f54392d5a2fcd4c394464d9b3f3d1f4cfc
Daniel Lessenden <dless27@gmail.com>
2024-04-07 12:52:28 -0600

    Make environment vars for the api

---
+++ src/environments/environments.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    // weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/denver/EN',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIHeaderValue: 'd1b5bf6169mshaaa869edd8d7fa2p1f0262jsne896b6d08c74'
+};
